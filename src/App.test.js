98f6d7aa008e48e5d4578be6b955d5528ec60e8a
Gaurav Kute <kuteg2915@gmail.com>
2026-01-29 21:02:07 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders inventory heading', () => {
+//   render(<App />);
+//   const heading = screen.getByText(/inventory/i);
+//   expect(heading).toBeInTheDocument();
+// });
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders inventory heading', () => {
+test('renders app', () => {
   render(<App />);
-  const heading = screen.getByText(/inventory/i);
-  expect(heading).toBeInTheDocument();
+  expect(screen.getByText(/learn react/i)).toBeInTheDocument();
 });
+
