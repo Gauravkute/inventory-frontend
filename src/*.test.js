cccf403e93ee4face49a588727c8195414f2dec9
Gaurav Kute <kuteg2915@gmail.com>
2026-01-30 08:48:13 +0530

    Delete src/*.test.js

--- src/*.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Hello from './Hello';
-
-test('renders greeting with name', () => {
-  render(<Hello name="Gaurav" />);
-  const greetingElement = screen.getByText(/Hello, Gaurav!/i);
-  expect(greetingElement).toBeInTheDocument();
-});
