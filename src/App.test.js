c43eeaf61d68698afbabc9e7b0902adcd02adc79
Gaurav Kute <kuteg2915@gmail.com>
2026-01-30 18:06:15 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,4 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
 
-// test('renders inventory heading', () => {
-//   render(<App />);
-//   const heading = screen.getByText(/inventory/i);
-//   expect(heading).toBeInTheDocument();
-// });
 // import { render, screen } from '@testing-library/react';
 // import App from './App';
 
